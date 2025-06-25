1df225ed87fa831da58a7bc78edac4af9f519ad3
Madhumitha <163529836+madhumitha1306@users.noreply.github.com>
2025-06-25 10:21:49 +0530

    Delete emp directory

--- emp/emp/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .user import User
-from .employee import Employee
-from .password_reset_otp import PasswordResetOTP
-
--- emp/emp/utils/common.py
+++
@@ -1,5 +0,0 @@
-# utils/common.py
-import uuid
-
-def generate_uuid():
-    return str(uuid.uuid4())
